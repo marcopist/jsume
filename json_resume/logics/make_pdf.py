cc8408adadb8492835a58aea6f23de9f1aa2e229
Marco Pistilli <39236511+marcopist@users.noreply.github.com>
2023-02-28 21:03:54 +0000

    Running jinja templates in sandbox

--- json_resume/logics/make_pdf.py
+++ json_resume/logics/make_pdf.py
@@ -2,7 +2,9 @@ from addict import Dict
 from jinja2 import Template
 import pdfkit
 from .github import get_theme_from_gist
+from jinja2.sandbox import SandboxedEnvironment
 
+sandbox = SandboxedEnvironment()
 
 def makepdf(resume_raw, theme=None):
     resume = Dict(resume_raw)
@@ -11,12 +13,9 @@ def makepdf(resume_raw, theme=None):
         author = resume.meta.author
         theme = resume.meta.theme
 
-    theme_jinja = get_theme_from_gist(author, theme)
+    template = get_theme_from_gist(author, theme)
 
-
-    template = Template(theme_jinja)
-
-    rendered = template.render(resume = resume)
+    rendered = sandbox.from_string(template).render(resume = resume)
 
     return pdfkit.from_string(
         rendered,
