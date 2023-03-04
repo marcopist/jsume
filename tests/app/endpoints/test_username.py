c9062b91259a5e481a26233cfb205c4d943611d4
Marco Pistilli <39236511+marcopist@users.noreply.github.com>
2023-03-04 18:13:29 +0000

    Added dockerfile for tests & imagemagick policy

--- tests/app/endpoints/test_username.py
+++ tests/app/endpoints/test_username.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import patch
 
 from json_resume.app.endpoints.username import route_username
+from ...utils.code.compare_pdf import compare_pdf
 
 def safe_get_gist(username, file):
     if file == 'resume.json':
@@ -14,4 +15,12 @@ def safe_get_gist(username, file):
 
 @patch("json_resume.logics.github.get_gist", safe_get_gist)
 def test_route_username():
-    return route_username('test_username')
+    with open("tests/utils/files/outputs/sample-resume.pdf", 'rb') as f:
+        expected_pdf = f.read()
+
+    actual_pdf = route_username('test_username').data
+
+    diff = compare_pdf(expected_pdf, actual_pdf)
+    
+    print(diff)
+    assert diff[1] < 0.001
