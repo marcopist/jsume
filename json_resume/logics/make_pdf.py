9986ddb844558e7cf71bf33ecda5aba62e263238
Marco Pistilli <39236511+marcopist@users.noreply.github.com>
2023-03-05 15:13:49 +0000

    :memo: added docs to make_pdf.py

--- json_resume/logics/make_pdf.py
+++ json_resume/logics/make_pdf.py
@@ -1,3 +1,5 @@
+"""Module containing the logics to build the pdf from
+a template theme and a parsed resume dictionary"""
 from addict import Dict
 import pdfkit
 from .github import get_theme
@@ -6,7 +8,21 @@ from json_resume.app.app import app
 
 sandbox = SandboxedEnvironment()
 
-def makepdf(resume_raw, author=None, theme=None):
+def makepdf(resume_raw : Dict, author: str = None, theme: str = None) -> bytes:
+    """Returns a rendered pdf based on a json resume `resume_raw`.
+    Extracts the theme information from the `resume_raw` input, but also
+    tolerates explicitly defined temem through the `author` and `theme` arguments.
+
+    Args:
+        resume_raw (addict.Dict): Resume data.
+        author (str, optional): Author of the theme to be used.
+            Defaults to None.
+        theme (str, optional): Name of the theme to be used.
+            Defaults to None.
+
+    Returns:
+        bytes: The bytes of the generated pdf.
+    """
     resume = Dict(resume_raw)
     app.logger.debug(f"{resume=}")
 
@@ -14,7 +30,7 @@ def makepdf(resume_raw, author=None, theme=None):
         author = resume.meta.author
         theme = resume.meta.theme
 
-    app.logger.info(f"Theme author {author}")
+    app.logger.info(f"Theme author = {author}")
     app.logger.info(f"Theme name = {theme}")
 
     template = get_theme(author, theme)
