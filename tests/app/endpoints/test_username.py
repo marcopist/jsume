b288e0883120278e001ed6638f6b623bc58c14fe
Marco Pistilli <39236511+marcopist@users.noreply.github.com>
2023-03-06 21:45:12 +0000

    :white_check_mark: Added test case for no published cv

--- tests/app/endpoints/test_username.py
+++ tests/app/endpoints/test_username.py
@@ -5,7 +5,24 @@ from json_resume.app.endpoints.username import route_username
 from ...utils.code.compare_pdf import compare_pdf
 
 
-def safe_get_gist(username, file):
+def safe_get_gist(username: str, file: str) -> str | None:
+    """Closure to mock github calls in json_resume.logics.github.get_gist
+
+    NOTE: if `username` == 'noresume' this mocks
+    the situation where an user has not published
+    a resume to github
+
+    Args:
+        username (str): The name of the Github organisation or user
+            who published the Gist.
+        file (str): The name of the published Gist.
+
+    Returns:
+        str | None: the raw file from Github if it exists,
+            otherwise it returns `None`.
+    """
+    if username == "noresume" and file == "resume.json":
+        return None
     if file == "resume.json":
         with open("tests/utils/files/inputs/sample-resume.json") as f:
             return f.read()
@@ -16,7 +33,18 @@ def safe_get_gist(username, file):
 
 
 @patch("json_resume.logics.github.get_gist", safe_get_gist)
-def test_route_username():
+def test_route_username_cv_N() -> None:
+    """Test for json_resume.app.endpoints.route_username
+    Assumes that no resume was published"""
+    actual = route_username("noresume")
+
+    assert actual == ""
+
+
+@patch("json_resume.logics.github.get_gist", safe_get_gist)
+def test_route_username_cv_Y() -> None:
+    """Test for json_resume.app.endpoints.route_username
+    Assumes that a resume was published"""
     with open("tests/utils/files/outputs/sample-resume.pdf", "rb") as f:
         expected_pdf = f.read()
 
@@ -24,5 +52,4 @@ def test_route_username():
 
     diff = compare_pdf(expected_pdf, actual_pdf)
 
-    print(diff)
-    assert diff[1] < 0.001
+    assert diff[1] < 0.01
